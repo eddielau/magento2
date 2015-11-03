8cf65dad26693f0b0d250177e0609d76dcd3ab1a
eddielau <eddielau224@gmail.com>
2015-11-03 16:08:27 -0600

    Update Dom.php

--- lib/internal/Magento/Framework/Config/Dom.php
+++ lib/internal/Magento/Framework/Config/Dom.php
@@ -276,6 +276,9 @@ class Dom
         $schema,
         $errorFormat = self::ERROR_FORMAT_DEFAULT
     ) {
+        if (!function_exists('libxml_set_external_entity_loader')) {
+            return [];
+        }
         if (!self::$urnResolver) {
             self::$urnResolver = new UrnResolver();
         }
